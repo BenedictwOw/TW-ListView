f0d7f396c4f786a919808ae194fdf0c5c001ffbd
18044928 <18044928@myrp.edu.sg>
2020-04-30 12:47:04 +0800

    fileFile2

--- app/src/main/java/com/myapplicationdev/android/tw_listview/Module.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/Module.java
@@ -1,14 +1,25 @@
 package com.myapplicationdev.android.tw_listview;
 
 
+import java.util.ArrayList;
+
 public class Module {
     private String year;
-    private String module;
+    private ArrayList module;
     private boolean prog;
 
-    public Module(String year,String module, boolean prog) {
+
+    public Module(String year,ArrayList module, boolean prog) {
         this.year = year;
         this.prog = prog;
+        this.module = module;
+    }
+
+    public ArrayList getModule() {
+        return module;
+    }
+
+    public void setModule(ArrayList module) {
         this.module = module;
     }
 
